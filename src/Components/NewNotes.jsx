a38720a720490144abf3cddf96b986ac0772ec07
pankajktech <pankajkatech@gmail.com>
2023-04-14 14:03:46 +0530

    Updated

--- src/Components/NewNotes.jsx
+++ src/Components/NewNotes.jsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { AddNOTE } from "../Redux/Action";
 import { useNavigate } from "react-router-dom";
+import "regenerator-runtime/runtime";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { NoteAdded } from "./Message";
@@ -12,6 +16,17 @@ const NewNotes = () => {
   const [isNoteAdded, setIsNoteAdded] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const startListenings = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const stopListenings = () => SpeechRecognition.stopListening();
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
   const AddNotes = (e) => {
     e.preventDefault();
@@ -39,7 +54,7 @@ const NewNotes = () => {
         My All Notes
         <AiOutlineArrowRight className="ml-2" />
       </button>
-      <div className=" w-[90%] lg:w-1/3 bg-slate-800 backdrop-blur-md bg-opacity-80 flex justify-center flex-col p-3  md:p-10 rounded-md drop-shadow-2xl ">
+      <div className=" w-[90%] lg:w-1/3 bg-slate-800 flex justify-center flex-col p-3  md:p-10 rounded-lg drop-shadow-2xl ">
         <h1 className="text-xl text-slate-200 border-b border-slate-100/25 mb-5 text-center w-full p-2">
           Add A New Note
         </h1>
@@ -57,13 +72,35 @@ const NewNotes = () => {
             placeholder="Enter The Description"
             className="textarea textarea-secondary textarea-ghost w-full h-60 my-5"
             value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            onChange={(e) => setDescription(e.target.value || { transcript })}
             required
           />
           <button type="submit" className="btn btn-secondary  w-full text-xl">
             Add Note
           </button>
         </form>
+      </div>
+      <div className="flex justify-center items-center">
+        <button
+          onClick={startListenings}
+          className="btn btn-active btn-info mt-5 mx-3"
+        >
+          Start
+        </button>
+        <button
+          onClick={stopListenings}
+          className="btn btn-active btn-info mt-5 mx-3"
+        >
+          Stop
+        </button>
+
+        <button
+          onClick={() => setDescription(transcript)}
+          className="btn btn-active btn-info mt-5 mx-3"
+        >
+          Add
+        </button>
+        <div className="text-white mt-5 mx-3 text-lg">{transcript}</div>
       </div>
     </div>
   );
